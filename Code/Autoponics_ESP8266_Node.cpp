d238a9b0f6944d220e61ae6df43e21f994a65be2
Claudio <claudio.ray5@gmail.com>
2020-01-08 12:50:34 +0100

    Node updated for Adafruit_IO_Client

--- Code/Autoponics_ESP8266_Node.cpp
+++ Code/Autoponics_ESP8266_Node.cpp
@@ -1,17 +1,25 @@
+// Adafruit IO REST API access with ESP8266
+//
+// For use with ESP8266 Arduino from:
+//   https://github.com/esp8266/Arduino
+//
+// Works great with ESP8266 modules like the Adafruit Huzzah ESP:
+//  ----> https://www.adafruit.com/product/2471
+//
+// Written by Tony DiCola for Adafruit Industries.  
+// MIT license, all text above must be included in any redistribution.
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include "Adafruit_IO_Client.h"
 
-// WiFi parameters
+
+// Configure WiFi access point details.
 #define WLAN_SSID       "Guaripolo"
 #define WLAN_PASS       "Internet1"
 
-// Adafruit IO
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "ClaudioRay"
+// Configure Adafruit IO access.
 #define AIO_KEY         "57b0c505622e76376f141c0f1e0e6447dca7d4cf"
-
+WiFiClient client; // Create an ESP8266 WiFiClient class to connect to the AIO server.
+Adafruit_IO_Client aio = Adafruit_IO_Client(client, AIO_KEY);
 
 //variables codigo obtencion
 char data[9];
@@ -29,7 +37,7 @@ char datai[6];
 char dataj[6];
 char datak[6];
 char datal[6];
-//char datam[6]; //correspondiente a EC
+char datam[6]; //correspondiente a EC
 //char datan[6]; //correspondiente a flujo superior
 //char datao[6]; //correspondiente a flujo inferior
 //char datap[6]; //correspondiente a flujo base
@@ -51,113 +59,49 @@ int dfi; // dato flujo inferior
 int dfb; // dato flujo base
 unsigned long currentMillis;
 unsigned long toggle=0;
-// Functions
-void connect();
 
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
+Adafruit_IO_Feed reinicio = aio.getFeed("reinicio");
+Adafruit_IO_Feed adahs = aio.getFeed("adahs");
+Adafruit_IO_Feed adats = aio.getFeed("adats");
+Adafruit_IO_Feed adahi = aio.getFeed("adahi");
+Adafruit_IO_Feed adati = aio.getFeed("adati");
+Adafruit_IO_Feed adaco2s = aio.getFeed("adaco2s");
+Adafruit_IO_Feed adaco2i = aio.getFeed("adaco2i");
+Adafruit_IO_Feed adaldrs = aio.getFeed("adaldrs");
+Adafruit_IO_Feed adaldri = aio.getFeed("adaldri");
+Adafruit_IO_Feed adat1 = aio.getFeed("adat1");
+Adafruit_IO_Feed adat2 = aio.getFeed("adat2");
+Adafruit_IO_Feed adat3 = aio.getFeed("adat3");
+Adafruit_IO_Feed adaph = aio.getFeed("adaph");
+Adafruit_IO_Feed adaec = aio.getFeed("adaec");
+Adafruit_IO_Feed adafs = aio.getFeed("adafs");
+Adafruit_IO_Feed adafi = aio.getFeed("adafi");
+Adafruit_IO_Feed adafb = aio.getFeed("adafb");
 
-// Store the MQTT server, client ID, username, and password in flash memory.
-const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
-
-// Set a unique MQTT client ID using the AIO key + the date and time the sketch
-// was compiled (so this should be unique across multiple devices for a user,
-// alternatively you can manually set this to a GUID or other random value).
-const char MQTT_CLIENTID[] PROGMEM  = AIO_KEY __DATE__ __TIME__;
-const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
-const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);/****************************** Feeds ***************************************/
-
-// Setup feeds
-
-const char ADAHS_FEED[] PROGMEM = AIO_USERNAME "/feeds/adahs";
-Adafruit_MQTT_Publish adahs = Adafruit_MQTT_Publish(&mqtt, ADAHS_FEED);
-
-const char ADATS_FEED[] PROGMEM = AIO_USERNAME "/feeds/adats";
-Adafruit_MQTT_Publish adats = Adafruit_MQTT_Publish(&mqtt, ADATS_FEED);
-
-const char ADAHI_FEED[] PROGMEM = AIO_USERNAME "/feeds/adahi";
-Adafruit_MQTT_Publish adahi = Adafruit_MQTT_Publish(&mqtt, ADAHI_FEED);
-
-const char ADATI_FEED[] PROGMEM = AIO_USERNAME "/feeds/adati";
-Adafruit_MQTT_Publish adati = Adafruit_MQTT_Publish(&mqtt, ADATI_FEED);
-
-const char ADACO2S_FEED[] PROGMEM = AIO_USERNAME "/feeds/adaco2s";
-Adafruit_MQTT_Publish adaco2s = Adafruit_MQTT_Publish(&mqtt, ADACO2S_FEED);
-
-const char ADACO2I_FEED[] PROGMEM = AIO_USERNAME "/feeds/adaco2i";
-Adafruit_MQTT_Publish adaco2i = Adafruit_MQTT_Publish(&mqtt, ADACO2I_FEED);
-
-const char ADALDRS_FEED[] PROGMEM = AIO_USERNAME "/feeds/adaldrs";
-Adafruit_MQTT_Publish adaldrs = Adafruit_MQTT_Publish(&mqtt, ADALDRS_FEED);
-
-const char ADALDRI_FEED[] PROGMEM = AIO_USERNAME "/feeds/adaldri";
-Adafruit_MQTT_Publish adaldri = Adafruit_MQTT_Publish(&mqtt, ADALDRI_FEED);
-
-const char ADAT1_FEED[] PROGMEM = AIO_USERNAME "/feeds/adat1";
-Adafruit_MQTT_Publish adat1 = Adafruit_MQTT_Publish(&mqtt, ADAT1_FEED);
-
-const char ADAT2_FEED[] PROGMEM = AIO_USERNAME "/feeds/adat2";
-Adafruit_MQTT_Publish adat2 = Adafruit_MQTT_Publish(&mqtt, ADAT2_FEED);
-
-const char ADAT3_FEED[] PROGMEM = AIO_USERNAME "/feeds/adat3";
-Adafruit_MQTT_Publish adat3 = Adafruit_MQTT_Publish(&mqtt, ADAT3_FEED);
-
-const char ADAPH_FEED[] PROGMEM = AIO_USERNAME "/feeds/adaph";
-Adafruit_MQTT_Publish adaph = Adafruit_MQTT_Publish(&mqtt, ADAPH_FEED);
-
-const char ADAEC_FEED[] PROGMEM = AIO_USERNAME "/feeds/adaec";
-Adafruit_MQTT_Publish adaec = Adafruit_MQTT_Publish(&mqtt, ADAEC_FEED);
-
-const char ADAFS_FEED[] PROGMEM = AIO_USERNAME "/feeds/adafs";
-Adafruit_MQTT_Publish adafs = Adafruit_MQTT_Publish(&mqtt, ADAFS_FEED);
-
-const char ADAFI_FEED[] PROGMEM = AIO_USERNAME "/feeds/adafi";
-Adafruit_MQTT_Publish adafi = Adafruit_MQTT_Publish(&mqtt, ADAFI_FEED);
-
-const char ADAFB_FEED[] PROGMEM = AIO_USERNAME "/feeds/adafb";
-Adafruit_MQTT_Publish adafb = Adafruit_MQTT_Publish(&mqtt, ADAFB_FEED);
-
-
-/*************************** Sketch Code ************************************/
+unsigned int count = 0;
 
 void setup() {
-
-
   Serial.begin(115200);
-  //Serial.println(F("Adafruit IO Example"));
-
-  // Connect to WiFi access point.
+  delay(5);
   Serial.println(); Serial.println();
-  delay(10);
-  Serial.print(F("Conectando con "));
+  Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
-
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(F("."));
+    Serial.print(".");
   }
   Serial.println();
-
-  Serial.println(F("Conectado mediante WiFi"));
-  Serial.println(F("Direccion IP: "));
-  Serial.println(WiFi.localIP());
-
-  // connect to adafruit io
-  connect();
-
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());  
+  aio.begin();
 }
 
-
-
-
-void loop() {
-
-          
-        if (Serial.available() > 0) 
+void loop() 
+{
+   
+  
+  if (Serial.available() > 0) 
         {
             dato = Serial.read();                
             data[i]=dato;
@@ -303,134 +247,120 @@ void loop() {
         }
      }
 
-
-     // se realiza un comparador millis ya que tener todo este segmento de codigo con un delay retrasa mucho la 
-     // recopilacion de datos. por lo que se define que los datos sean subidos cada 2 minutos 
-       
- currentMillis = millis();     
+FeedData latest = reinicio.receive();
+  if (latest.isValid()) 
+  {
+    Serial.print(F("Leer: ")); Serial.println(latest);
+    // By default the received feed data item has a string value, however you
+    // can use the following functions to attempt to convert it to a numeric
+    // value like an int or float.  Each function returns a boolean that indicates
+    // if the conversion succeeded, and takes as a parameter by reference the
+    // output value.
+    int i;
+    if (latest.intValue(&i)) 
+    {
+      Serial.print(F("Valor: ")); Serial.println(i, DEC);
+    }
+    // Other functions that you can use include:
+    //  latest.uintValue() (unsigned int)
+    //  latest.longValue() (long)
+    //  latest.ulongValue() (unsigned long)
+    //  latest.floatValue() (float)
+    //  latest.doubleValue() (double)
+  }
+  else 
+  {
+    Serial.print(F("Failed to receive the latest feed value!"));
+  }
+  
+  currentMillis = millis();     
  if(currentMillis - toggle >= 120000)
     { 
       toggle = currentMillis;  
       
       // se realiza ping a adafruit para coonfirmar la conexion
-      if(! mqtt.ping(3)) 
-      {
-        // en el caso de no estar conectados, se solicita la conexion
-        if(! mqtt.connected())
-          connect();
-      }
+
       
     // con lo anterior, se suben los datos
       //DHS
-       if (! adahs.publish(dhs))
+       if (! adahs.send(dhs))
         Serial.println(F("Error en publicar la Humedad Superior"));
       else
         Serial.println(F("Humedad Superior Publicada!"));
       //DTS 
-      if (! adats.publish(dts))
+      if (! adats.send(dts))
         Serial.println(F("Error en publicar la Temperatura Superior"));
       else
         Serial.println(F("Temperatura Superior Publicada!"));
        // DHI
-       if (! adahi.publish(dhi))
+       if (! adahi.send(dhi))
         Serial.println(F("Error en publicar la Humedad Inferior"));
       else
         Serial.println(F("Humedad Inferior Publicada!")); 
         //DTI
-      if (! adati.publish(dti))
+      if (! adati.send(dti))
         Serial.println(F("Error en publicar la Temperatura Inferior"));
       else
         Serial.println(F("Temperatura Inferior Publicada!"));
         //DCO2S
-      if (! adaco2s.publish(dco2s))
+      if (! adaco2s.send(dco2s))
         Serial.println(F("Error en la publicacion de CO2 Superior"));
       else
         Serial.println(F("CO2 Superior Publicado!"));
         //DCO2I
-      if (! adaco2i.publish(dco2i))
+      if (! adaco2i.send(dco2i))
         Serial.println(F("Error en la publicacion de CO2 Inferior"));
       else
         Serial.println(F("CO2 Inferior Publicado!"));
         //DLDRS
-          if (! adaldrs.publish(dldrs))
+          if (! adaldrs.send(dldrs))
         Serial.println(F("Error en la Publicacion del LDR Superior"));
       else
         Serial.println(F("LDR Superior Publicado!"));
         //DLDRI
-          if (! adaldri.publish(dldri))
+          if (! adaldri.send(dldri))
         Serial.println(F("Error en la Publicacion del LDR Inferior"));
       else
         Serial.println(F("LDR Inferior Publicado!"));
         //DT1
-      if (! adat1.publish(dt1))
+      if (! adat1.send(dt1))
         Serial.println(F("Error en la Publicacion de T1"));
       else
         Serial.println(F("T1 Publicado!"));
         //DT2
-        if (! adat2.publish(dt2))
+        if (! adat2.send(dt2))
         Serial.println(F("Error en la Publicacion de T2"));
       else
         Serial.println(F("T2 Publicado!"));
         //DT2
-        if (! adat3.publish(dt3))
+        if (! adat3.send(dt3))
         Serial.println(F("Error en la Publicacion de T3"));
       else
         Serial.println(F("T3 Publicado!"));
         //DPH
-        if (! adaph.publish(dph))
+        if (! adaph.send(dph))
         Serial.println(F("Error en Publicar pH"));
       else
         Serial.println(F("pH Publicado!"));
         //DEC
-        if (! adaec.publish(dec))
+        if (! adaec.send(dec))
         Serial.println(F("Error en Publicar EC"));
       else
         Serial.println(F("EC Publicado!"));
         //DFS
-        if (! adafs.publish(dfs))
+        if (! adafs.send(dfs))
         Serial.println(F("Error en Publicar Flujo Superior"));
       else
         Serial.println(F("Flujo Superior Publicado!"));
         //DFI
-        if (! adafi.publish(dfi))
+        if (! adafi.send(dfi))
         Serial.println(F("Error en Publicar Flujo Inferior"));
       else
         Serial.println(F("Flujo Inferior Publicado!"));
         //DFB
-        if (! adafb.publish(dfb))
+        if (! adafb.send(dfb))
         Serial.println(F("Error en Publicar Flujo Base"));
       else
         Serial.println(F("Flujo Base Publicado!"));          
     }
-}
-
-// connect to adafruit io via MQTT
-void connect() {
-
-  Serial.print(F("Conectando con Adafruit.io... "));
-
-  int8_t ret;
-
-  while ((ret = mqtt.connect()) != 0) {
-
-    switch (ret) {
-      case 1: Serial.println(F("Protocolo Incorrecto")); break;
-      case 2: Serial.println(F("ID Rechazada")); break;
-      case 3: Serial.println(F("Server No Disponible")); break;
-      case 4: Serial.println(F("Usuario/Clave Incorrectas")); break;
-      case 5: Serial.println(F("No Autorizado")); break;
-      case 6: Serial.println(F("Error en la Subscripcion")); break;
-      default: Serial.println(F("Error en la Conexion")); break;
-    }
-
-    if(ret >= 0)
-      mqtt.disconnect();
-
-    Serial.println(F("Reconectando..."));
-    delay(5000);
-
-  }
-
-  Serial.println(F("Conectado con Adafruit.io!"));
-
 }
